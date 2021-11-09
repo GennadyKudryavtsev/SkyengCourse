1e31129261071a48c12224854b528c09892166fd
GennadyKudryavtsev <gennady1504@yandex.ru>
2021-11-09 18:05:22 +0300

    Первый коммит

---
+++ src/main/java/ru/kudryavtsev/skyeng/SkyengCourseApplication.java
@@ -0,0 +1,13 @@
+package ru.kudryavtsev.skyeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyengCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyengCourseApplication.class, args);
+    }
+
+}
